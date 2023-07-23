6448849bd02c4306adedba66b09c6a76dd7bc23e
Cr0m4 <david.valero@usach.cl>
2023-07-22 22:22:24 -0400

    preguntas-service creado

---
+++ preguntas-service/src/main/java/com/example/preguntasservice/PreguntasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.preguntasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreguntasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreguntasServiceApplication.class, args);
+	}
+
+}
---
+++ preguntas-service/src/test/java/com/example/preguntasservice/PreguntasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preguntasservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguntasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
